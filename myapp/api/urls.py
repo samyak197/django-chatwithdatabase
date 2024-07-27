1b5e8110a573db275278426ae7c79cbfc3a68ecc
Samyak Nahar <142987873+samyak197@users.noreply.github.com>
2024-07-28 01:50:42 +0530

    Update urls.py

--- myapp/api/urls.py
+++ myapp/api/urls.py
@@ -1,7 +1,7 @@
 # api/urls.py
 
 from django.urls import path
-from .views import GenAIView, ChatWithPdf, ChatWithDB
+from .views import GenAIView, ChatWithPdf, ChatWithDB, ChatWithDBCJ
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -9,4 +9,5 @@ urlpatterns = [
     path("genai/", GenAIView.as_view(), name="genai"),
     path("chatpdf/", ChatWithPdf.as_view(), name="chatpdf"),
     path("chatdb/", ChatWithDB.as_view(), name="chatdb"),
+    path("chatdbcj/", ChatWithDBCJ.as_view(), name="chatdbcj"),
 ]
